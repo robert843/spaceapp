fe5a4ca7d35c4caee9acfc2522ec5c48fc5bb105
Hefajstos <mail_jk@icloud.com>
2018-10-20 18:38:05 +0200

    Class for building stars

---
+++ achivements/achivements.js
@@ -0,0 +1,121 @@
+// var STARS = [
+//     {
+//         name: 'name1',
+//         visited: false,
+//         coordinates: { x: 10, y: 10 },
+//         color: any,
+//         img: 'link',
+//         description: 'desc'
+//     }
+// ]
+
+var STARS = new Array();
+
+class Star {
+    constructor (starName, starCoordinates, starColor, starImg, starDescription) {
+        this.starName = starName;
+        this.starCoordinates = starCoordinates;
+        this.starColor = starColor;
+        this.starImg = starImg;
+        this.starDescription = starDescription;
+    }
+
+    get dataJSON() {
+        return this.generateDataForSave();
+    }
+
+    generateDataForSave() {
+        let star = new Object();
+
+        star.name = this.starName;
+        star.visited = false;
+        star.coordinates = this.starCoordinates;
+        star.color = this.starColor;
+        star.img = this.starImg;
+        star.description = this.starDescription;
+
+        return star;
+    }
+
+    get getStarDescription() {
+        return this.starDescription;
+    }
+
+    get getStarThumbnail() {
+        return this.generateStarThumbnail();
+    }
+
+    generateStarThumbnail() {
+        let thumbnail = document.createElement('li'); 
+        let thumbnailImg = "url('" + this.starImg + "')";
+
+        thumbnail.classList.add('exploredElements');
+        thumbnail.style.backgroundImage = thumbnailImg;
+        return thumbnail;
+    }
+
+}
+
+STARS.push(
+    new Star(
+        'Star1', 
+        { 
+            x: 10, 
+            y: 10 
+        }, 
+        '#fff',
+        "http://placehold.it/400x400?text=generated+1",
+        "Lorem ipsum dolor sit 1"
+    )
+)
+
+STARS.push(
+    new Star(
+        'Star2', 
+        { 
+            x: 10, 
+            y: 10 
+        }, 
+        '#fff',
+        "http://placehold.it/400x400?text=generated+2",
+        "Lorem ipsum dolor sit 2"
+    )
+)
+
+STARS.push(
+    new Star(
+        'Star3', 
+        { 
+            x: 10, 
+            y: 10 
+        }, 
+        '#fff',
+        "http://placehold.it/400x400?text=generated+3",
+        "Lorem ipsum dolor sit 3"
+    )
+)
+
+STARS.push(
+    new Star(
+        'Star4', 
+        { 
+            x: 10, 
+            y: 10 
+        }, 
+        '#fff',
+        "http://placehold.it/400x400?text=generated+4",
+        "Lorem ipsum dolor sit 4"
+    )
+)
+
+function LoadStars(StarsArray) {
+    let numberOfDiscoveredStars = StarsArray.length;
+    let starsDisplayBox = document.getElementById('exploredElements');
+
+    for (var i = 0; i < numberOfDiscoveredStars; i++) {
+        starsDisplayBox.appendChild(StarsArray[i].getStarThumbnail);
+    }
+
+}
+
+LoadStars(STARS);
